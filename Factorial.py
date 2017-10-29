f6dba52e6b3952991eb15b7a22400b3b787d6a86
cmccand1 <clint.mccandless@gmail.com>
2017-10-29 01:58:39 -0600

    Add files via upload

--- Factorial.py
+++ Factorial.py
@@ -1,1 +1,12 @@
+# A recursive function that returns the factorial of a number
+user_input = input("Enter a number: ")
 
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print factorial(user_input)
