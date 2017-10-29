b69c0b49ea67820e0b686aea69f8c9282ec9b104
cmccand1 <clint.mccandless@gmail.com>
2017-10-29 00:06:26 -0600

    Update IterativeBinarySearch.py

--- IterativeBinarySearch.py
+++ IterativeBinarySearch.py
@@ -1,1 +1,21 @@
-def binary_search(key, a):        lo = 0    hi = len(a) - 1    found = False    while lo <= hi and not found:        mid = lo + (hi - lo) / 2        if key == a[mid]:            found = True        if key < a[mid]:            hi = mid - 1        if key > a[mid]:            lo = mid + 1    return foundarray = [x for x in range(0, 101)]print arrayprint binary_search(37, array)
+def binary_search(key, a):
+    
+    lo = 0
+    hi = len(a) - 1
+    found = False
+
+    while lo <= hi and not found:
+        mid = lo + (hi - lo) / 2
+        if key == a[mid]:
+            found = True
+        if key < a[mid]:
+            hi = mid - 1
+        if key > a[mid]:
+            lo = mid + 1
+
+    return found
+
+
+array = [x for x in range(0, 101)]
+print array
+print binary_search(37, array)
